cf4e7e2c6d7617e2e9f33aee9acd75d22d7adae1
FrontendSuberu <joeysuberu@gmail.com>
2026-03-25 21:15:47 +0100

    logic re arranged for better clarity

--- src/main.js
+++ src/main.js
@@ -18,20 +18,20 @@ function initRippleHover() {
       item.classList.add("hover"); // Add .hover to current item
 
       // Toggle classes for siblings
-      toggleSiblingClass(navItems, index, -1, "sibling-close", true); // Previous sibling
       toggleSiblingClass(navItems, index, 1, "sibling-close", true); // Next sibling
-      toggleSiblingClass(navItems, index, -2, "sibling-far", true); // Previous-previous sibling
       toggleSiblingClass(navItems, index, 2, "sibling-far", true); // Next-next sibling
+      toggleSiblingClass(navItems, index, -1, "sibling-close", true); // Previous sibling
+      toggleSiblingClass(navItems, index, -2, "sibling-far", true); // Previous-previous sibling
     });
 
     item.addEventListener("mouseleave", () => {
       item.classList.remove("hover"); // Remove .hover from current item
 
       // Toggle classes for siblings
-      toggleSiblingClass(navItems, index, -1, "sibling-close", false); // Previous sibling
       toggleSiblingClass(navItems, index, 1, "sibling-close", false); // Next sibling
-      toggleSiblingClass(navItems, index, -2, "sibling-far", false); // Previous-previous sibling
       toggleSiblingClass(navItems, index, 2, "sibling-far", false); // Next-next sibling
+      toggleSiblingClass(navItems, index, -1, "sibling-close", false); // Previous sibling
+      toggleSiblingClass(navItems, index, -2, "sibling-far", false); // Previous-previous sibling
     });
   });
 }
